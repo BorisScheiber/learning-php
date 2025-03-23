7402249c657c7a122f42160966d926b892ec6bb3
Boris Scheiber <scheiberboris@hotmail.com>
2025-03-23 17:16:30 +0100

    update container service

--- controllers/notes/index.php
+++ controllers/notes/index.php
@@ -1,15 +1,13 @@
 <?php
 
+use Core\App;
 use Core\Database;
 
-$config = require base_path('config.php');
-$db = new Database($config['database']);
-
+$db = App::resolve(Database::class);
 
 
 $notes = $db->query('select * from notes where user_id = 1')->get();
 
-//dd($notes);
 
 
 view("notes/index.view.php", [
