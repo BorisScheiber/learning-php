343d4e8feb23105a1a9930afe8eda65bd5267318
Boris Scheiber <scheiberboris@hotmail.com>
2025-03-28 01:37:55 +0100

    Session.php + Autenticator.php implemented

--- Core/functions.php
+++ Core/functions.php
@@ -37,51 +37,9 @@ function view($path, $attributes = [])
     require base_path('views/' . $path);
 }
 
-function login($user) {
-    $_SESSION['user'] = [
-        'email' => $user['email']
-    ];
-
-    session_regenerate_id(true);
+function redirect($path)
+{
+    header("Location: {$path}");
+    exit();
 }
-
-function logout() {
-
-    $_SESSION = [];
-    session_destroy();
-
-    $params = session_get_cookie_params();
-    setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
-
-}
-
-
-
-// PHP Superglobals Referenz:
-
-// 1. $_SERVER: Infos zu Server, URL und Request
-// Beispiel: Aktuelle URL erhalten
-// $currentUrl = $_SERVER['REQUEST_URI'];
-
-// 2. $_GET: URL-Parameter (z.B. ?name=Max)
-// Beispiel: URL-Parameter abrufen
-// $name = $_GET['name'];
-
-// 3. $_POST: Daten aus Formularen (bei POST-Methode)
-// Beispiel: Formularfeld abrufen
-// $email = $_POST['email'];
-
-// 4. $_SESSION: Daten, die zwischen mehreren Seitenaufrufen gespeichert werden
-// Beispiel: Wert in Session speichern und abrufen
-// $_SESSION['user_id'] = 123;
-// $userId = $_SESSION['user_id'];
-
-// 5. $_COOKIE: Zugriff auf gespeicherte Cookies
-// Beispiel: Cookie abrufen
-// $loginToken = $_COOKIE['login_token'];
-
-// 6. $_FILES: Infos über hochgeladene Dateien
-// Beispiel: Name der hochgeladenen Datei abrufen
-// $filename = $_FILES['my_file']['name'];
-
 
